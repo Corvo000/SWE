94da4777a03767c86d19e32121eea0b9e3688e82
Corvo000 <hendlpaul1998@gmail.com>
2020-04-30 15:17:50 +0200

    sda

---
+++ sweTemp/src/SWE/FroehlicheZahl.java
@@ -0,0 +1,16 @@
+package SWE;
+
+public class FroehlicheZahl 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int i=0;i<51;i++)
+			if(!isHappy(i))
+				System.out.println(i);
+	}
+	public static boolean isHappy(int n) 
+	{
+		return false;
+	}
+}
